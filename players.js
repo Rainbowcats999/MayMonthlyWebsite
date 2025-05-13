7328f993b058a892b319d91d63491baabd217afa
Rainbowcats999 <526erizov@frhsd.com>
2025-05-13 08:55:25 -0400

    Added the comma after each object

--- players.js
+++ players.js
@@ -1,37 +1,44 @@
 const players = [
-    {
-        firstName: 'Aleksander',
-        lastName:'Barkov',
-        position:'center',
-        age: '29',
-        photo: 'https://picsum.photos/',
-    }
-    {
-        firstName: 'Aleksander',
-        lastName:'Barkov',
-        position:'',
-        age: '',
-        photo: 'https://picsum.photos/',
-    }
-    {
-        firstName: 'Aleksander',
-        lastName:'Barkov',
-        position:'',
-        age: '',
-        photo: 'https://picsum.photos/',
-    }
-    {
-        firstName: 'Aleksander',
-        lastName:'Barkov',
-        position:'',
-        age: '',
-        photo: 'https://picsum.photos/',
-    }
-    {
-        firstName: 'Aleksander',
-        lastName:'Barkov',
-        position:'',
-        age: '',
-        photo: 'https://picsum.photos/',
-    }
+  {
+    firstName: "Aleksander",
+    lastName: "Barkov",
+    position: "Center",
+    age: "29",
+    photo: "https://picsum.photos/",
+  },
+  {
+    firstName: "Evan",
+    lastName: " Rodrigues",
+    position: "Center",
+    age: "31",
+    photo: "https://picsum.photos/",
+  },
+  {
+    firstName: "Aleksander",
+    lastName: "Barkov",
+    position: "",
+    age: "",
+    photo: "https://picsum.photos/",
+  },
+  {
+    firstName: "Aleksander",
+    lastName: "Barkov",
+    position: "",
+    age: "",
+    photo: "https://picsum.photos/",
+  },
+  {
+    firstName: "Aleksander",
+    lastName: "Barkov",
+    position: "",
+    age: "",
+    photo: "https://picsum.photos/",
+  },
+  {
+    firstName: "Aleksander",
+    lastName: "Barkov",
+    position: "",
+    age: "",
+    photo: "https://picsum.photos/",
+  },
 ];
