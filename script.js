b5a160254ea70e7e2881466f1929da6d36289722
Rainbowcats999 <526erizov@frhsd.com>
2025-05-29 10:43:47 -0400

    Started working on the modal... ty Kyle

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.getElementById("rosterGrid");
 
+  grid.addEventListener("click", function (e) {
+    if (e.target.classList.contains("show-info-btn")) {
+      const index = e.target.getAttribute("data-player-index");
+      const player = players[index];
+      showPlayerModal(player);
+    }
+  });
+
+  function showPlayerModal(player) {
+    document.getElementById("modalPhoto").src = player.photo;
+    document.getElementById(
+      "modalName"
+    ).textContent = `${player.firstName} ${player.lastName}`;
+    document.getElementById("modalPosition").textContent = player.position;
+    document.getElementById("modalAge").textContent = player.age;
+  }
+
   const render = (list) => {
     grid.innerHTML = "";
 
-    list.forEach((p) => {
+    list.forEach((p, index) => {
       const col = document.createElement("div");
 
       col.className =
@@ -18,11 +35,12 @@ document.addEventListener("DOMContentLoaded", () => {
             <h5 class="card-title mb-1"> ${p.firstName}<br>${p.lastName} 
             </h5>
             <div class="badge badge-position badge-pos-${p.position}">
-
+              <h5 class="text-black">${p.position}</h5>
             </div>
             <p class="small text-muted mb-0">
-                Age${p.age}
+                Age: ${p.age}
             </p>
+            <button class="btn btn-sm btn-primary my-3 show-info-btn" data-player-index="${index}" data-bs-toggle="modal" data-bs-target="#playerModal">Click Here</button>
         </div>
         </div>
     </div>
