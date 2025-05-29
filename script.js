2afcf04a910bf38cebc1d348471f652f7764a1d7
Rainbowcats999 <526erizov@frhsd.com>
2025-05-29 10:10:15 -0400

    Added a nice cool border around the cards

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
     list.forEach((p) => {
       const col = document.createElement("div");
 
-      col.className = "col-6 col-lg-2";
+      col.className =
+        "col-6 col-md-3 col-xl-2 d-flex justify-content-center align-items-center";
 
       col.innerHTML = `
       <div class="card h-100 shadow-sm">
-        <img src="${p.photo}" class="card-img-top" alt="${p.firstName} ${p.lastName}">
+      <div class="card-content">
+        <img src="${p.photo}" class="card-img-top" alt="${p.firstName}${p.lastName}">
         <div class="card-body-top text-center">
-            <h5 class="card-title mb-1"> ${p.firstName} ${p.lastName} 
+            <h5 class="card-title mb-1"> ${p.firstName}<br>${p.lastName} 
             </h5>
             <div class="badge badge-position badge-pos-${p.position}">
 
@@ -21,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <p class="small text-muted mb-0">
                 Age${p.age}
             </p>
+        </div>
         </div>
     </div>
       `;
@@ -34,6 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function transition() {
   //transition animations would go here
-  alert("transition");
+  //alert("transition");
   window.location.href = "players.html";
 }
